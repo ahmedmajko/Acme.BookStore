17e0f306e9ff66846e276748e63712100a39f60b
ahmed hassan <ahmedmajko@gmail.com>
2024-12-31 17:25:48 +0300

    Add project files.

---
+++ src/Acme.BookStore.Domain/Categories/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Entities.Auditing;
+
+namespace Acme.BookStore.Categories
+{
+    public class Category:FullAuditedEntity<int>
+    {
+        public string NameAr { get; set; }
+        public string NameEn { get; set; }
+        public string DescroptionAr { get; set; }
+        public string DescroptionEn { get; set; }
+    }
+}
---
+++ src/Acme.BookStore.Domain/Products/Product.cs
@@ -0,0 +1,21 @@
+﻿using Acme.BookStore.Categories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Entities.Auditing;
+
+namespace Acme.BookStore.Products
+{
+    public class Product: FullAuditedEntity<int>
+    {
+        public string NameAr { get; set; }
+        public string NameEn { get; set; }
+        public string DescroptionAr { get; set; }
+        public string DescroptionEn { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+
+    }
+}
